45683084e77b612be7dc90f92b6f0d9c9602169c
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2013-06-06 17:48:52 +0300

    Removed comments

--- tests/test_table_builder.py
+++ tests/test_table_builder.py
@@ -1,29 +1,28 @@
 import sqlalchemy as sa
 from sqlalchemy_continuum import Versioned
-from sqlalchemy_continuum.table_builder import VersionedTableBuilder
 from tests import TestCase
 
 
-# class TestTableBuilder(TestCase):
-#     def test_assigns_foreign_keys_for_versions(self):
-#         article = self.Article()
-#         article.name = u'Some article'
-#         article.content = u'Some content'
-#         article.tags.append(self.Tag(name=u'some tag'))
-#         self.session.add(article)
-#         self.session.commit()
-#         cls = self.Tag.__versioned__['class']
-#         version = self.session.query(cls).first()
-#         assert version.name == u'some tag'
-#         assert version.id == 1
-#         assert version.article_id == 1
+class TestTableBuilder(TestCase):
+    def test_assigns_foreign_keys_for_versions(self):
+        article = self.Article()
+        article.name = u'Some article'
+        article.content = u'Some content'
+        article.tags.append(self.Tag(name=u'some tag'))
+        self.session.add(article)
+        self.session.commit()
+        cls = self.Tag.__versioned__['class']
+        version = self.session.query(cls).first()
+        assert version.name == u'some tag'
+        assert version.id == 1
+        assert version.article_id == 1
 
-#     def test_versioned_table_structure(self):
-#         table = self.Article.__versioned__['class'].__table__
-#         assert 'id' in table.c
-#         assert 'name' in table.c
-#         assert 'content' in table.c
-#         assert 'description'in table.c
+    def test_versioned_table_structure(self):
+        table = self.Article.__versioned__['class'].__table__
+        assert 'id' in table.c
+        assert 'name' in table.c
+        assert 'content' in table.c
+        assert 'description'in table.c
 
 
 class TestTableBuilderWithColumnOrderInspection(TestCase):
