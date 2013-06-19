eb99668a30fb7722b8cecce7c76ef461a537874d
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2013-06-19 15:47:04 +0300

    Bumped version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ class PyTest(Command):
 
 setup(
     name='SQLAlchemy-Continuum',
-    version='0.4.2',
+    version='0.5.0',
     url='https://github.com/kvesteri/sqlalchemy-continuum',
     license='BSD',
     author='Konsta Vesterinen',
