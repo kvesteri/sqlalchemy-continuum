830364b13ab8a3840382fe1bad3616c34daf0690
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2013-10-18 12:51:38 +0300

    More tests for vacuum

--- tests/test_vacuum.py
+++ tests/test_vacuum.py
@@ -17,6 +17,12 @@ class TestVacuum(TestCase):
                 name=u'Some article',
                 transaction_id=2,
                 operation_type=1
+            ),
+            self.ArticleHistory(
+                id=1,
+                name=u'Some article',
+                transaction_id=3,
+                operation_type=1
             )
         ]
 
@@ -26,3 +32,27 @@ class TestVacuum(TestCase):
         vacuum(self.session, self.Article)
         assert history_objects[0] not in self.session.deleted
         assert history_objects[1] in self.session.deleted
+        assert history_objects[2] in self.session.deleted
+
+    def test_does_not_delete_versions_with_actual_changes(self):
+        history_objects = [
+            self.ArticleHistory(
+                id=1,
+                name=u'Some article',
+                transaction_id=1,
+                operation_type=1
+            ),
+            self.ArticleHistory(
+                id=1,
+                name=u'Some other article',
+                transaction_id=2,
+                operation_type=1
+            )
+        ]
+
+        self.session.add_all(history_objects)
+        self.session.commit()
+
+        vacuum(self.session, self.Article)
+        assert history_objects[0] not in self.session.deleted
+        assert history_objects[1] not in self.session.deleted
