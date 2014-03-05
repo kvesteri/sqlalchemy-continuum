08c5951423f65d225016c607ba6d9fe775c6ac1b
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2014-03-05 21:38:49 +0200

    Add tests for NullDeletePlugin

---
+++ tests/plugins/test_null_delete.py
@@ -0,0 +1,30 @@
+from sqlalchemy_continuum.plugins import NullDeletePlugin
+from tests import TestCase
+
+
+class DeleteTestCase(TestCase):
+    def _delete(self):
+        article = self.Article()
+        article.name = u'Some article'
+        article.content = u'Some content'
+        self.session.add(article)
+        self.session.commit()
+
+        self.session.delete(article)
+        self.session.commit()
+
+    def test_stores_operation_type(self):
+        self._delete()
+        versions = self.session.query(self.ArticleHistory).all()
+        assert versions[1].operation_type == 2
+
+
+class TestDeleteWithoutStoreDataAtDelete(DeleteTestCase):
+    plugins = [NullDeletePlugin]
+
+    def test_creates_versions_on_delete(self):
+        self._delete()
+        versions = self.session.query(self.ArticleHistory).all()
+        assert len(versions) == 2
+        assert versions[1].name is None
+        assert versions[1].content is None
