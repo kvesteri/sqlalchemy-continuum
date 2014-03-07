8c62bbd08ffd3924d512bd40628fd92a6e246e18
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2014-03-07 09:39:49 +0200

    Refactor tests

--- tests/test_one_to_many_relations.py
+++ tests/test_one_to_many_relations.py
@@ -11,6 +11,30 @@ class OneToManyRelationshipsTestCase(TestCase):
         self.session.commit()
         assert article.versions[0].tags.count()
 
+    def test_insert_related_object(self):
+        article = self.Article()
+        article.name = u'Some article'
+        article.content = u'Some content'
+        self.session.add(article)
+        self.session.commit()
+
+        self.session.add(self.Tag(name=u'some tag', article=article))
+        self.session.commit()
+        assert article.versions.count() == 1
+
+    def test_relationships_for_history_objects(self):
+        article = self.Article()
+        article.name = u'Some article'
+        article.content = u'Some content'
+        article.tags.append(self.Tag(name=u'some tag'))
+        self.session.add(article)
+        self.session.commit()
+        version = article.versions.all()[0]
+        assert version.name == u'Some article'
+        assert version.content == u'Some content'
+        version = article.tags[0].versions.all()[0]
+        assert version.name == u'some tag'
+
     def test_consecutive_inserts_and_removes(self):
         article = self.Article()
         article.name = u'Some article'
