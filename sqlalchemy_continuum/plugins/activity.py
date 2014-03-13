8b2cd84e7eebe5dfe3709ce2cf3f3422d8b9047b
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2014-03-13 10:57:20 +0200

    Add more docs for activity

--- sqlalchemy_continuum/plugins/activity.py
+++ sqlalchemy_continuum/plugins/activity.py
@@ -9,16 +9,18 @@ You can initalize the ActivityPlugin by adding it to versioning manager.
 
 ::
 
-    make_versioned(plugins=[ActivityPlugin])
+    activity_plugin = ActivityPlugin()
+
+    make_versioned(plugins=[activity_plugin])
 
 
 ActivityPlugin uses single database table for tracking activities. This table
 follows the data structure in `activity stream specification`_, but it comes
 with a nice twist:
 
-    ==============  =========== ========================================
+    ==============  =========== =============
     Column          Type        Description
-    --------------  ----------- ----------------------------------------
+    ==============  =========== =============
     id              BigInteger  The primary key of the activity
     verb            Unicode     Verb defines the action of the activity
     data            JSON        Additional data for the activity in JSON format
@@ -39,16 +41,56 @@ with a nice twist:
 
     target_tx_id    BigInteger  The last transaction_id associated with the
                                 target.
-    ==============  =========== ========================================
+    ==============  =========== =============
 
 
 Each Activity has relationships to actor, object and target but it also holds
-information about the transaction when it was created. This allows each
-activity to also have object_version and target_version relationships for
-introspecting what those objects and targets were in given point in time.
+information about the associated transaction and about the last associated
+transactions with the target and object. This allows each activity to also have
+object_version and target_version relationships for introspecting what those
+objects and targets were in given point in time. All these relationship
+properties use `generic relationships`_ of the SQLAlchemy-Utils package.
+
+
+
+Once your models have been configured you can get the Activity model from the
+ActivityPlugin class with activity_cls property::
+
+
+    Activity = activity_plugin.activity_cls
+
+
+Now let's say we have model called Article and Category. Each Article has one
+Category. Activities should be created along with the changes you make on
+these models::
+
+    article = Article(name=u'Some article')
+    session.add(article)
+    first_activity = Activity(verb=u'create', object=article)
+    session.add(first_activity)
+    session.commit()
+
+
+The object property of the Activity object holds the current object and the
+object_version holds the object version at the time when the activity was
+created.
+
+
+    article.name = u'Some article updated!'
+    second_activity = Activity(verb=u'update', object=article)
+    session.add(second_activity)
+    session.commit()
+
+    second_activity.object.name  # u'Some article updated!'
+    first_activity.object.name  # u'Some article updated!'
+
+    first_activity.object_version.name  # u'Some article'
+
 
 .. _activity stream specification:
     http://www.activitystrea.ms
+.. _generic relationships:
+    http://sqlalchemy-utils.readthedocs.org/en/latest/generic_relationship.html
 """
 
 import sqlalchemy as sa
@@ -170,8 +212,8 @@ class ActivityFactory(ModelFactory):
 
 class ActivityPlugin(Plugin):
     def after_build_models(self, manager):
-        self.model_class = ActivityFactory(manager)()
-        manager.activity_cls = self.model_class
+        self.activity_cls = ActivityFactory(manager)()
+        manager.activity_cls = self.activity_cls
 
     def is_session_modified(self, session):
         """
@@ -180,11 +222,11 @@ class ActivityPlugin(Plugin):
 
         :param session: SQLAlchemy session object
         """
-        return any(isinstance(obj, self.model_class) for obj in session)
+        return any(isinstance(obj, self.activity_cls) for obj in session)
 
     def before_flush(self, uow, session):
         for obj in session:
-            if isinstance(obj, self.model_class):
+            if isinstance(obj, self.activity_cls):
                 obj.transaction = uow.current_transaction
 
     def after_version_class_built(self, parent_cls, version_cls):
