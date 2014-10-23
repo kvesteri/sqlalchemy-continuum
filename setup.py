976d5bdebc84cb79a152fa8c216c07b9b579e079
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2014-10-23 12:45:36 +0300

    Remove cmdclass

--- setup.py
+++ setup.py
@@ -6,10 +6,9 @@ Versioning and auditing extension for SQLAlchemy.
 """
 
 import os
-import subprocess
 import sys
 import re
-from setuptools import setup, Command
+from setuptools import setup
 
 
 HERE = os.path.dirname(os.path.abspath(__file__))
@@ -22,21 +21,6 @@ def get_version():
         contents = f.read()
     pattern = r"^__version__ = '(.*?)'$"
     return re.search(pattern, contents, re.MULTILINE).group(1)
-
-
-
-class PyTest(Command):
-    user_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        errno = subprocess.call(['py.test'])
-        raise SystemExit(errno)
 
 
 extras_require = {
@@ -86,7 +70,6 @@ setup(
         if sys.version_info[0] == 2 and sys.version_info[1] < 7 else ''
     ],
     extras_require=extras_require,
-    cmdclass={'test': PyTest},
     classifiers=[
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
