8c24d2b7ed2613fea70048b8355aa6c5759678ed
Martin Marrese <marrese@gmail.com>
2015-12-17 10:01:59 -0300

    Option to use module name on version class name
    Add new option in VersionManager that will tell ModelBuiler if it should use
    the module name when creating Version tables or not. This allow using classes
    with the same name but on different modules without collisions with the
    version classes.

--- sqlalchemy_continuum/model_builder.py
+++ sqlalchemy_continuum/model_builder.py
@@ -252,11 +252,13 @@ class ModelBuilder(object):
 
         args.update(self.get_inherited_denormalized_columns(table))
 
-        return type(
-            '%sVersion' % self.model.__name__,
-            self.base_classes(),
-            args
-        )
+        if self.manager.options.get('use_module_name', True):
+            name = '{}{}Version'.format(
+                self.model.__module__.title().replace('.', ''),
+                self.model.__name__)
+        else:
+            name = '{}Version'.format(self.model.__name__)
+        return type(name, self.base_classes(), args)
 
     def __call__(self, table, tx_class):
         """
--- tests/utils/test_version_class.py
+++ tests/utils/test_version_class.py
@@ -1,5 +1,7 @@
 from pytest import raises
 from sqlalchemy_continuum import ClassNotVersioned, version_class
+from sqlalchemy_continuum.manager import VersioningManager
+from sqlalchemy_continuum.model_builder import ModelBuilder
 
 from tests import TestCase
 
@@ -12,3 +14,10 @@ class TestVersionClass(TestCase):
     def test_throws_error_for_non_versioned_class(self):
         with raises(ClassNotVersioned):
             version_class('invalid')
+
+    def test_module_name_in_class_name(self):
+        options = {'use_module_name': True}
+        vm = VersioningManager(options=options)
+        mb = ModelBuilder(vm, self.Article)
+        ArticleVersion = mb.build_model(self.Article.__table__)
+        assert ArticleVersion.__name__ == 'TestsArticleVersion'
