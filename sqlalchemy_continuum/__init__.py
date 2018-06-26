679036254813296478657daa7a89496f0deb8261
Colton Allen <cmanallen90@gmail.com>
2018-06-26 11:26:21 -0700

    Revert "Merge pull request #167 from netcriptus/hotfix/track-cloned-connections"
    This reverts commit 8658a8924429a7e3699274170d83d38ee12e636a, reversing
    changes made to c4aad39608c3dd98b7f0091012656a5a657166ab.

--- sqlalchemy_continuum/__init__.py
+++ sqlalchemy_continuum/__init__.py
@@ -70,12 +70,6 @@ def make_versioned(
         manager.track_association_operations
     )
 
-    sa.event.listen(
-        sa.engine.Engine,
-        'set_connection_execution_options',
-        manager.track_cloned_connections
-    )
-
 
 def remove_versioning(
     mapper=sa.orm.mapper,
@@ -101,10 +95,4 @@ def remove_versioning(
         sa.engine.Engine,
         'before_cursor_execute',
         manager.track_association_operations
-    )
-
-    sa.event.remove(
-        sa.engine.Engine,
-        'set_connection_execution_options',
-        manager.track_cloned_connections
     )
