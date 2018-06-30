832078f32f81443feb8498624d36a7378e51cc34
Colton Allen <cmanallen90@gmail.com>
2018-06-29 17:27:07 -0700

    Add non-versioned many-to-many test case

--- tests/relationships/test_many_to_many_relations.py
+++ tests/relationships/test_many_to_many_relations.py
@@ -194,6 +194,172 @@ class ManyToManyRelationshipsTestCase(TestCase):
 create_test_cases(ManyToManyRelationshipsTestCase)
 
 
+class NonVersionedManyToManyRelationshipsTestCase(TestCase):
+    def create_models(self):
+        class Article(self.Model):
+            __tablename__ = 'article'
+            __versioned__ = {
+                'base_classes': (self.Model, )
+            }
+
+            id = sa.Column(sa.Integer, autoincrement=True, primary_key=True)
+            name = sa.Column(sa.Unicode(255))
+
+        article_tag = sa.Table(
+            'article_tag', self.Model.metadata,
+            sa.Column('article_id', sa.Integer, sa.ForeignKey('article.id'), primary_key=True),
+            sa.Column('tag_id', sa.Integer, sa.ForeignKey('tag.id'), primary_key=True))
+
+        class Tag(self.Model):
+            __tablename__ = 'tag'
+
+            id = sa.Column(sa.Integer, autoincrement=True, primary_key=True)
+            name = sa.Column(sa.Unicode(255))
+
+        Tag.articles = sa.orm.relationship(
+            Article,
+            secondary=article_tag,
+            backref='tags'
+        )
+
+        self.Article = Article
+        self.Tag = Tag
+        self.article_tag = article_tag
+
+    def test_version_relations(self):
+        tag1 = self.Tag()
+        self.session.add(tag1)
+
+        tag2 = self.Tag()
+        self.session.add(tag2)
+
+        article = self.Article()
+        article.tags = [tag1]
+        self.session.add(article)
+        self.session.commit()
+
+        assert len(article.tags) == 1
+        assert len(article.versions[0].tags) == 1
+
+    def test_single_insert(self):
+        article = self.Article()
+        article.name = u'Some article'
+        article.content = u'Some content'
+        tag = self.Tag(name=u'some tag')
+        article.tags.append(tag)
+        self.session.add(article)
+        self.session.commit()
+        assert len(article.versions[0].tags) == 1
+
+    def test_multi_insert(self):
+        article = self.Article()
+        article.name = u'Some article'
+        article.content = u'Some content'
+        tag = self.Tag(name=u'some tag')
+        article.tags.append(tag)
+        article.tags.append(self.Tag(name=u'another tag'))
+        self.session.add(article)
+        self.session.commit()
+        assert len(article.versions[0].tags) == 2
+
+    def test_collection_with_multiple_entries(self):
+        article = self.Article()
+        article.name = u'Some article'
+        article.content = u'Some content'
+        self.session.add(article)
+        article.tags = [
+            self.Tag(name=u'some tag'),
+            self.Tag(name=u'another tag')
+        ]
+        self.session.commit()
+        assert len(article.versions[0].tags) == 2
+
+    def test_delete_single_association(self):
+        article = self.Article()
+        article.name = u'Some article'
+        article.content = u'Some content'
+        tag = self.Tag(name=u'some tag')
+        article.tags.append(tag)
+        self.session.add(article)
+        self.session.commit()
+        article.tags.remove(tag)
+        article.name = u'Updated name'
+        self.session.commit()
+        tags = article.versions[1].tags
+        assert len(tags) == 0
+
+    def test_delete_multiple_associations(self):
+        article = self.Article()
+        article.name = u'Some article'
+        article.content = u'Some content'
+        tag = self.Tag(name=u'some tag')
+        tag2 = self.Tag(name=u'another tag')
+        article.tags.append(tag)
+        article.tags.append(tag2)
+        self.session.add(article)
+        self.session.commit()
+        article.tags.remove(tag)
+        article.tags.remove(tag2)
+        article.name = u'Updated name'
+        self.session.commit()
+        assert len(article.versions[1].tags) == 0
+
+    def test_remove_node_but_not_the_link(self):
+        article = self.Article()
+        article.name = u'Some article'
+        article.content = u'Some content'
+        tag = self.Tag(name=u'some tag')
+        article.tags.append(tag)
+        self.session.add(article)
+        self.session.commit()
+        self.session.delete(tag)
+        article.name = u'Updated name'
+        self.session.commit()
+        tags = article.versions[1].tags
+        assert len(tags) == 0
+
+    def test_relations_with_varying_transactions(self):
+        if (
+            self.driver == 'mysql' and
+            self.connection.dialect.server_version_info < (5, 6)
+        ):
+            pytest.skip()
+
+        # one article with one tag
+        article = self.Article(name=u'Some article')
+        tag1 = self.Tag(name=u'some tag')
+        article.tags.append(tag1)
+        self.session.add(article)
+        self.session.commit()
+
+        # update article and tag, add a 2nd tag
+        tag2 = self.Tag(name=u'some other tag')
+        article.tags.append(tag2)
+        tag1.name = u'updated tag1'
+        article.name = u'updated article'
+        self.session.commit()
+
+        # update article and first tag only
+        tag1.name = u'updated tag1 x2'
+        article.name = u'updated article x2'
+        article.tags = []
+        self.session.add(article)
+        self.session.commit()
+
+        article.name = u'test'
+        article.tags.append(tag2)
+        self.session.add(article)
+        self.session.commit()
+
+        assert len(article.versions[0].tags) == 1
+        assert len(article.versions[1].tags) == 2
+        assert len(article.versions[2].tags) == 0
+        assert len(article.versions[3].tags) == 1
+
+
+create_test_cases(NonVersionedManyToManyRelationshipsTestCase)
+
+
 class TestManyToManyRelationshipWithViewOnly(TestCase):
     def create_models(self):
         class Article(self.Model):
