ecf4fc33db7f8c2974b4ba4b01b6a8dd91cf3471
Neyts Zupan <nejc.zupan@gmail.com>
2023-02-11 10:58:56 +0000

    Declare six as a dependency
    It seems that older versions of SQLAlchemy and SQLAlchemy-Utils depended
    on six, so there was no need to declare it here.
    
    However, latest releases of SQLAlchemy and SQLAlchemy-Utils no longer
    depend on six, but SQLAlchemy-Continuum does in fact
    [import six](https://github.com/kvesteri/sqlalchemy-continuum/blob/master/sqlalchemy_continuum/operation.py#L7),
    so we need to declare it as a dependency.

--- setup.py
+++ setup.py
@@ -64,7 +64,8 @@ setup(
     platforms='any',
     install_requires=[
         'SQLAlchemy>=1.0.8',
-        'SQLAlchemy-Utils>=0.30.12'
+        'SQLAlchemy-Utils>=0.30.12',
+        'six',
     ],
     extras_require=extras_require,
     classifiers=[
