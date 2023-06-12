0aeca4aa505a0d512e22fe04f7b713f37118d073
Mark Steward <marksteward@gmail.com>
2023-06-12 21:54:17 +0100

    Test deadlock for multiple objects and log thread ID

--- tests/test_validity_strategy_multithreaded.py
+++ tests/test_validity_strategy_multithreaded.py
@@ -3,17 +3,29 @@ import sqlalchemy as sa
 from sqlalchemy import func
 from sqlalchemy.orm import sessionmaker
 from tests import TestCase
-from threading import Thread
+from threading import Thread, current_thread
+from logging import getLogger, Formatter
 
 
 NUM_ROWS = 100
 NUM_THREADS = 4
 
 
+class ThreadFormatter(Formatter):
+    thread_formatter = Formatter('%(threadName)s ')
+
+    def __init__(self, inner):
+        self.inner = inner
+
+    def format(self, record):
+        return self.thread_formatter.format(record) + self.inner.format(record)
+
+
 class TestValidityStrategyMultithreaded(TestCase):
     class WrappedThread(Thread):
         """
-        Wrapper around `threading.Thread` that propagates exceptions.
+        Wrapper around `threading.Thread` that propagates exceptions
+        and includes threadName in SQLAlchemy log lines.
 
         REF: https://gist.github.com/sbrugman/59b3535ebcd5aa0e2598293cfa58b6ab
         """
@@ -23,10 +35,24 @@ class TestValidityStrategyMultithreaded(TestCase):
             self.exc = None
 
         def run(self):
+            logger = getLogger('sqlalchemy.engine.Engine')
+
+            formatters = {}
+            handlers = []
+            for handler in logger.handlers:
+                if isinstance(handler.formatter, ThreadFormatter):
+                    continue
+                formatters.setdefault(handler.formatter, ThreadFormatter(handler.formatter))
+                handler.setFormatter(formatters[handler.formatter])
+                handlers.append(handler)
+
             try:
                 super().run()
             except BaseException as e:
                 self.exc = e
+
+            for handler in handlers:
+                handler.setFormatter(handler.formatter.inner)
 
         def join(self, timeout=None):
             super().join(timeout)
@@ -47,9 +73,9 @@ class TestValidityStrategyMultithreaded(TestCase):
         self.Article = Article
 
     @pytest.mark.skipif("os.environ.get('DB') == 'sqlite'")
-    def test_for_deadlock_with_many_multithreaded_inserts(self):
+    def test_single_objects(self):
         threads = [
-            self.WrappedThread(target=self._insert_articles, args=(n,))
+            self.WrappedThread(target=self._insert_update_single_article)
             for n in range(0, NUM_THREADS)
         ]
 
@@ -60,15 +86,51 @@ class TestValidityStrategyMultithreaded(TestCase):
             t.join()
 
 
-    def _insert_articles(self, num):
+    def _insert_update_single_article(self):
         Session = sessionmaker(bind=self.engine.connect())
         session = Session(autoflush=False)
+        name = current_thread().name
         for i in range(1, NUM_ROWS+1):
-            article = self.Article(name=f'Article {num}-{i:04}')
+            article = self.Article(name=f'Article {name}-{i:04}')
             session.add(article)
             session.commit()
-            article.name = article.name + '.2'
+            article.name += '.2'
             session.commit()
         assert session.query(func.count(self.Article.id)). \
-               where(self.Article.name.like(f'Article {num}-%')). \
+               where(self.Article.name.like(f'Article {name}-%')). \
                scalar() == NUM_ROWS
+
+
+    @pytest.mark.skipif("os.environ.get('DB') == 'sqlite'")
+    def test_multiple_objects(self):
+        threads = [
+            self.WrappedThread(target=self._insert_update_multiple_articles)
+            for n in range(0, NUM_THREADS)
+        ]
+
+        for t in threads:
+            t.start()
+
+        for t in threads:
+            t.join()
+
+
+    def _insert_update_multiple_articles(self):
+        Session = sessionmaker(bind=self.engine.connect())
+        session = Session(autoflush=False)
+        name = current_thread().name
+        for i in range(1, NUM_ROWS+1):
+            article1 = self.Article(name=f'Article 1 {name}-{i:04}')
+            article2 = self.Article(name=f'Article 2 {name}-{i:04}')
+            session.add_all([article1, article2])
+            session.commit()
+            article1.name += '.2'
+            article2.name += '.2'
+            session.commit()
+        assert session.query(func.count(self.Article.id)). \
+               where(self.Article.name.like(f'Article 1 {name}-%')). \
+               scalar() == NUM_ROWS
+        assert session.query(func.count(self.Article.id)). \
+               where(self.Article.name.like(f'Article 2 {name}-%')). \
+               scalar() == NUM_ROWS
+
